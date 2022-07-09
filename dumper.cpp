22570784986137cca94bb4a4009ddfb67c2037b7
Eve <84185604+evil33333333@users.noreply.github.com>
2022-07-09 01:31:46 +0100

    Update dumper.cpp
    I did a bit more research and came across the "PyMarshal_WriteObjectToFile" function.

--- dumper.cpp
+++ dumper.cpp
@@ -1,10 +1,12 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
-#include "pch.h"
 #include <Python.h>
+#include <marshal.h>
 #include <TlHelp32.h>
 #include <fstream>
 #include <array>
 #include <iostream>
+#include <vector>
+#include <memory>
+#include <string>
 
 DWORD GetProcessId(std::wstring);
 
@@ -19,12 +21,13 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 
         HWND hwnd = GetConsoleWindow();
 
+
         // Add something the process name will contain 
         DWORD proc_id = GetProcessId(std::wstring(L"main.exe"));
         HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, false, proc_id);
 
         // offset of the pyobject you want to dump
-        uint64_t offset = 0x1C5EC0425E0;
+        uint64_t offset = 0x0000023FDBEEF0F0;
         PyObject* object = reinterpret_cast<PyObject*>(offset);
 
         size_t bytes_read;
@@ -36,28 +39,13 @@ BOOL APIENTRY DllMain( HMODULE hModule,
         if (ReadProcessMemory(handle, (LPVOID)(offset), (LPVOID)buffer.data(), sizeof(PyObject*), &bytes_read))
         {
             std::cout << "\n[PyObjDmp] Valid offset was read." << std::endl;
-
-            PyObject* repr = PyObject_Repr(object);
-            PyObject* str = PyUnicode_AsEncodedString(repr, "utf-8", "~E~");
-            const char* bytes = PyBytes_AS_STRING(str);
             
-            
-            if (!bytes)
-            {
-                MessageBeep(MB_ICONERROR);
-                std::cout << "[PyObjDmp] PyObject* Returned NULL." << std::endl;
-                (void)MessageBoxA(hwnd, "Failure getting the PyObject pointer | NULL BYTES", "PyObjDmp", MB_ICONERROR);
-            }
+            std::string filename = "0x" + std::to_string(offset) + ".bin";
 
-            else
-            {
-                std::cout << "[PyObjDmp] Received " << strlen(bytes) << "bytes from this PyObject." << std::endl;
-                std::ofstream file("dumped_pyobject.bin");
-                file.write(bytes, strlen(bytes));
-                file.close();
-
-                (void)MessageBoxA(hwnd, "Successfully Dumped PyObject", "PyObjDmp", MB_OK);
-            }
+            FILE* file = fopen(filename.c_str(), "wb");
+            PyMarshal_WriteObjectToFile(object, file, Py_MARSHAL_VERSION);
+            fclose(file);
+            (void)MessageBoxA(hwnd, "Successfully Dumped PyObject", "PyObjDmp", MB_OK);
         }
         // If it can't, obviously we cannot get it.
         else
